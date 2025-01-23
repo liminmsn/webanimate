6cb239543b99673d058d91328c38a8f566326b3e
杨再涛 <1772081518@qq.com>
2025-01-23 18:15:44 +0800

    初始化

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  server: {
+    port: 3030, // 设置开发服务器的端口号
+    open: true, // 自动打开浏览器
+  },
+  build: {
+    outDir: 'dist', // 构建输出目录
+    assetsDir: 'assets', // 静态资源目录
+    sourcemap: true, // 生成 source map 文件
+  },
+});
